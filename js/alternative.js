69d6a421b62af4dce962095c6e63b79cf8621a11
Marcus Twichel <marc.twichy@gmail.com>
2017-11-06 18:41:39 -0700

    Added first third of over/short calc

--- js/alternative.js
+++ js/alternative.js
@@ -1,17 +1,46 @@
 // JavaScript Document
-$("#gs").on("keyup", function(){
+$(".currency").on("input", function(){
 		"use strict";
     var valid = /^\d{0,6}(\.\d{0,2})?$/.test(this.value),
         val = this.value;
     
     if(!valid){
-        console.log("Invalid input!");
-        this.value = val.substring(0, val.length - 1);
+       this.value = val.substring(0, val.length - 1);
     }
 });
 
-$('#gs').on('input', function() {
+$('.currency').on('input', function() {
     "use strict";
-	var val = $('#gs').val();
-	$('#total').text(val);
+	var gs = 0.0; var gcs = 0.0; var gcr = 0.0;
+	
+	if($('#gs').val()===""){
+		gs = 0.0;
+	}else{
+		gs = parseFloat($('#gs').val());
+		
+	}
+	
+	if($('#gcs').val()===""){
+		gcs = 0.0;
+	}else{
+		gcs = parseFloat($('#gcs').val());
+	}
+	
+	if($('#gcr').val()===""){
+		gcr = 0.0;
+	}else{
+		gcr = parseFloat($('#gcr').val());
+	}
+	 
+	 
+	 
+	var netGross = gs + gcs - gcr;
+	console.log(gcs);
+	$('#net-gross').text("$ ".concat(round(netGross)));
 });
+
+
+  function round(value) {
+	 "use strict";
+	  return (Math.round(value*100))/100;
+  }
