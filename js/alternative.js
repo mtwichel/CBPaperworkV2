705200c23684dbe3b72c4e67250b237310981d21
Marcus Twichel <marc.twichy@gmail.com>
2017-11-06 19:08:24 -0700

    Finished first draft of over/short

--- js/alternative.js
+++ js/alternative.js
@@ -11,13 +11,12 @@ $(".currency").on("input", function(){
 
 $('.currency').on('input', function() {
     "use strict";
-	var gs = 0.0; var gcs = 0.0; var gcr = 0.0;
+	var gs; var gcs; var gcr; var d; var v; var visa; var mc; var amex; var disc; var pdout;
 	
 	if($('#gs').val()===""){
 		gs = 0.0;
 	}else{
 		gs = parseFloat($('#gs').val());
-		
 	}
 	
 	if($('#gcs').val()===""){
@@ -31,12 +30,59 @@ $('.currency').on('input', function() {
 	}else{
 		gcr = parseFloat($('#gcr').val());
 	}
-	 
-	 
+	
+	if($('#d').val()===""){
+		d = 0.0;
+	}else{
+		d = parseFloat($('#d').val());
+	}
+	
+	if($('#v').val()===""){
+		v = 0.0;
+	}else{
+		v = parseFloat($('#v').val());
+	}
+	
+	if($('#visa').val()===""){
+		visa = 0.0;
+	}else{
+		visa = parseFloat($('#visa').val());
+	}
+	
+	if($('#mc').val()===""){
+		mc = 0.0;
+	}else{
+		mc = parseFloat($('#mc').val());
+	}
+	
+	if($('#amex').val()===""){
+		amex = 0.0;
+	}else{
+		amex = parseFloat($('#amex').val());
+	}
+	
+	if($('#disc').val()===""){
+		disc = 0.0;
+	}else{
+		disc = parseFloat($('#disc').val());
+	}
+	
+	if($('#pdout').val()===""){
+		pdout = 0.0;
+	}else{
+		pdout = parseFloat($('#pdout').val());
+	}
+	
 	 
 	var netGross = gs + gcs - gcr;
+	var netSales = netGross - d - v;
+	var overShort = netSales - visa - mc - amex - disc - pdout;
+	
 	console.log(gcs);
 	$('#net-gross').text("$ ".concat(round(netGross)));
+	$('#net-sales').text("$ ".concat(round(netSales)));
+	$('#over-short').text("$ ".concat(round(overShort)));
+
 });
 
 
